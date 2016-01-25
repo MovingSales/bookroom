f3d1cab1f4c5939b4b91b7c0729447fde7980a57
femi oni <freefony@gmail.com>
2016-01-25 23:53:04 +0100

    complete signin function

--- src/app/users/users.signup.controller.js
+++ src/app/users/users.signup.controller.js
@@ -2,18 +2,28 @@
 
   'use strict';
 
-  var signUpCtrl = function (userService, authService){
+  var signUpCtrl = function (userService, authService, $state){
 
     var vm = this;
     vm.user = {};
 
     vm.signup = function (){
       authService.signup(vm.user.email, vm.user.password)
-        .then(function (response){
+        .then(function (){
           return authService.login(vm.user.email, vm.user.password)
         })
         .catch(function (err) {
           console.error(err);
+        })
+    };
+
+    vm.signin = function (email, password){
+      return authService.login(email, password)
+        .then(function (){
+          $state.go('home')
+        })
+        .catch(function (err){
+          console.log(err);
         })
     }
   };
