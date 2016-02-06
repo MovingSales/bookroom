d71cbbf2bb87952ae9fd18d61f79f626bb44db84
femi oni <freefony@gmail.com>
2016-02-06 23:14:05 +0100

    user.home dir

---
+++ src/app/users/home/user.home.ctrl.js
@@ -0,0 +1,13 @@
+/**
+ * @ngdoc controller
+ * @name user-home
+ *
+ * @description
+ * _Please update the description and dependencies._
+ *
+ * @requires $scope
+ * */
+angular.module('user.home')
+    .controller('UserHomeCtrl', function(){
+
+});
